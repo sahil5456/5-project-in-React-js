7bec63e10d2edc02275523f823e029a26e97f594
Sahil Patel <jonyydeep555@gmail.com>
2024-02-16 15:46:43 +0530

    by sahil

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import  Restaurants  from "./componet/basics/Restaurants";
+// import  Restaurants  from "./componet/basics/Restaurants";
 // import UseState from "./componet/basics/hooks/UseState";
 // import UseEffect from "./componet/basics/hooks/UseEffect";
 // import UseReducer from "./componet/basics/hooks/UseReducer";
-// import TodoList from "./componet/listdata/TodoList";
+import TodoList from "./componet/listdata/TodoList";
 // import Temp from "./componet/weather/Temp";
 
 const App = () => {
-  return <Restaurants/>;
+  return <TodoList/>;
 };
 
 export default App;
